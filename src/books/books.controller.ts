cb176091dc21a7da20170363ab54771f40a55492
Sławomir Toporek <toporek.slawomir@gmail.com>
2024-07-15 22:06:12 +0200

    Add the getAll endpoint for the books module

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -1,7 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { BooksService } from './books.service';
 
 @Controller('books')
 export class BooksController {
   constructor(private booksService: BooksService) {}
+
+  @Get('/')
+  getAll(): any {
+    return this.booksService.getAll();
+  }
 }
--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -1,7 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { Book } from '@prisma/client';
 import { PrismaService } from 'src/shared/services/prisma.service';
 
 @Injectable()
 export class BooksService {
   constructor(private prismaService: PrismaService) {}
+
+  public getAll(): Promise<Book[]> {
+    return this.prismaService.book.findMany({
+      include: { author: true },
+    });
+  }
 }
