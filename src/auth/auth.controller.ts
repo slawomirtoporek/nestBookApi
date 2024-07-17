866233686c35d754abc5d25c4cac934abafb333b
Sławomir Toporek <toporek.slawomir@gmail.com>
2024-07-17 22:05:11 +0200

    Add the register endpoint to the auth module and add the Match method to utils

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { RegisterDTO } from './dtos/register.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
+
+  @Post('/register')
+  register(@Body() registrationData: RegisterDTO) {
+    return this.authService.register(registrationData);
+  }
 }
